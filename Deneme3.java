efce46e955fda5c00ff5edbb946bcbc348052dde
AlperenSagutdev <140455296+AlperenSagutdev@users.noreply.github.com>
2023-09-26 20:16:16 +0300

    Add files via upload

---
+++ Deneme3.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Deneme3 {
+    public static void main(String[] args) {
+        int total = 0;
+        int n;
+        Scanner input = new Scanner(System.in);
+
+        do {
+            System.out.print("Sayı Giriniz: ");
+            n = input.nextInt();
+            if ( n % 4 == 0) {
+                total +=n;
+
+            } else {
+                System.out.println("Tek sayı Girdiniz");
+                break;
+            }
+        } while (n % 2 == 0);
+        System.out.println("Çift ve 4 e bölünebilen tam sayıların Toplamı: "+total);
+
+
+    }
+}
